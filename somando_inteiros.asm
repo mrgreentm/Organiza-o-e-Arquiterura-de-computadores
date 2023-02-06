b3e9a8f70dc073b105f79e7a60cfe4a3e1f2f665
joao souza <joaonetotfm@gmail.com>
2023-02-05 21:12:00 -0300

    feature: adiciona comentarios

--- somando_inteiros.asm
+++ somando_inteiros.asm
@@ -1,6 +1,6 @@
 .text
-	li $t0, 48
-	li $t1, 12
-	add $a0,$t0,$t1
-	li $v0, 1
-	syscall
+	li $t0, 48 # atribui ao registrador $t0 o inteiro 48
+	li $t1, 12 # atribui ao registrador $t1 o inteiro 12
+	add $a0,$t0,$t1 # atribui ao registrador $a0 a soma dos registradores $t0 e $t1
+	li $v0, 1 # imprime um inteiro
+	syscall # chama
