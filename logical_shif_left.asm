57bf18f45de07cd85d29e55d2f13609c502590ad
joao souza <joaonetotfm@gmail.com>
2023-02-05 21:56:03 -0300

    feature: shift left

---
+++ logical_shif_left.asm
@@ -0,0 +1,4 @@
+.text
+	li $t1, 3 # adicionar o valor 3 ao registrador $t1
+	sll $t0, $t1, 2 # adiciona ao registrador $t0 o valor do registrador $t1 movido duas casas para esquerda em binário, ou seja, vezes 2^2
+	
