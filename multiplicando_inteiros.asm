f63a22dc61a42aca12fb5cc032aeb75b07e32f66
joao souza <joaonetotfm@gmail.com>
2023-02-05 21:32:34 -0300

    feature: multiplicando inteiros

---
+++ multiplicando_inteiros.asm
@@ -0,0 +1,4 @@
+.text
+	li $t0, 12 # atribui ao registrador $t0 o valor 12
+	li $t1, 2 # atribui ao registrador $t1 o valor 2
+	mul $s0, $t0,$t1 # atribui ao registrador $s0 o valor da multiplicacao entre os registradores $t0 e $t1
