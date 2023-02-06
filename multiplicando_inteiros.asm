28c9bed46d6dfc5687da5f329782089649ca5bc4
joao souza <joaonetotfm@gmail.com>
2023-02-05 21:46:31 -0300

    feature: mult inteiros e move registradores

--- multiplicando_inteiros.asm
+++ multiplicando_inteiros.asm
@@ -2,3 +2,5 @@
 	li $t0, 12 # atribui ao registrador $t0 o valor 12
 	li $t1, 2 # atribui ao registrador $t1 o valor 2
 	mul $s0, $t0,$t1 # atribui ao registrador $s0 o valor da multiplicacao entre os registradores $t0 e $t1
+	li $v0, 1 # imprime um inteiro
+	move $a0, $s0 # move o valor armazenado no registrador $s0 e coloca no registrador $a0
